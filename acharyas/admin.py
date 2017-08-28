f50561dc0cdeaf50ca0dd1dda1416d6a19f63182
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-08-28 16:33:13 -0400

    added acharya and center models

--- acharyas/admin.py
+++ acharyas/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Centre, Acharya
 
-# Register your models here.
+admin.site.register(Acharya)
+admin.site.register(Centre)
--- acharyas/models.py
+++ acharyas/models.py
@@ -12,15 +12,17 @@ class Centre(models.Model):
     country = CountryField()
     postal_code = models.CharField(max_length=10, blank=True)
     continent = models.CharField(max_length=100, blank=True)
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.country, )
 
 class Acharya(models.Model):
-    profile_id = models.CharField(max_length=100, blank=True, default='')
-    salutation = models.CharField(max_length=100, blank=True, default='')
+    profile_id = models.CharField(max_length=100, blank=True)
+    salutation = models.CharField(max_length=100, blank=True)
     name = models.CharField(max_length=100)
-    dob = models.DateField()
-    centre = models.ManyToManyField(Centre, related_name='acharya')
-    joined_date = models.DateField()
-    br_diksha_date = models.DateField()
+    dob = models.DateField(blank=True)
+    centre = models.ManyToManyField(Centre, related_name='acharya', blank=True)
+    joined_date = models.DateField(blank=True)
+    br_diksha_date = models.DateField(blank=True)
     trained_under = models.ForeignKey("self")
     def __str__(self):
         return self.name
